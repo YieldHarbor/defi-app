1e74a419887c210b52855df0ba60c1eaa567b6bc
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-19 13:26:23 +0530

    Stake Component created

---
+++ packages/client/src/pages/index.js
@@ -0,0 +1,15 @@
+
+
+import Stake from "../components/Stake"
+
+import MainLayout from '@/layouts/mainLayout';
+
+export default function Home() {
+
+  return (
+    <MainLayout>
+      <Stake/>
+      
+    </MainLayout>
+  )
+}
