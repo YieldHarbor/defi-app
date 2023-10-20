00eb5bc1be4875ca61e0256f1779e4a4806696d0
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-20 16:32:22 +0530

    Constants added

---
+++ packages/client/src/constants.js
@@ -0,0 +1,44 @@
+
+
+export const PACKAGE_ID = "0x9c22e4ec6439f67b4bd1c84c9fe7154969e4c88fe1b414602c1a4d56a54209f6"
+// export const TREASURY_CAP = "0x8ae56684e4541aa5d920f3e08ceedb5927797818e420474e1b7a3b6ef28730a0"
+export const MANAGER_CAP = "0x0de35f6af0cff8071e01b0134ad598e6016f5ceb0476fc3cd9f43753c966f714"
+export const RESERVE = "0x50c6f8cf9745a96a7f066afa00633fc06b6f6c33ba7aba4c49de521f07eacf4c"
+// export const MARKETPLACE = ""
+export const AMM = "0xa9724fe7e32a71c977325817de7a56e4613b41008baa98979c17958c80b30572"
+
+export const TYPE = "0x89b77424c9514f64537f83ae5e260286ee08f03bbc723cf1cc15c601cea9fb8d::staked_sui::STAKED_SUI"
+
+export const VAULT = [
+    {
+        id: 1,
+        name: '1 Day',
+        symbol : "HYT-SUI-22-OCT-23",
+        avatar:
+            './sui-sui-logo.svg',
+    },
+    {
+        id: 2,
+        name: '1 Week ',
+        symbol: "HYT-SUI-29-OCT-23",
+        ytSymbol: "YT-SUI-29-OCT-23",
+        avatar:
+            './sui-sui-logo.svg',
+    },
+    {
+        id: 3,
+        name: '1 Month',
+        symbol: "HYT-SUI-22-Nov-23",
+        ytSymbol: "YT-SUI-22-Nov-23",
+        avatar:
+            './sui-sui-logo.svg',
+    },
+    {
+        id: 4,
+        name: '1 Year',
+        symbol: "HYT-SUI-22-OCT-24",
+        ytSymbol: "YT-SUI-22-OCT-24",
+        avatar:
+            './sui-sui-logo.svg',
+    },
+]
